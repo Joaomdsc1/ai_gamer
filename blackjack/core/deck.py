c1dd83f119516c6f0fdb7c782dcbd947fc455f9c
Joao Guilherme <jgmarcondes34@gmail.com>
2026-02-02 17:43:12 -0300

    criação da função de embaralhar o baralho

--- blackjack/core/deck.py
+++ blackjack/core/deck.py
@@ -1,3 +1,4 @@
+import random
 
 class Baralho:
     def __init__(self):
@@ -5,11 +6,17 @@ class Baralho:
         self.naipes = ['♠', '♥', '♦', '♣']
         self.baralho = []
         self.criar_baralho()
+        self.shuffle()
+
 
     def criar_baralho(self):
         for i in self.valores:
             for j in self.naipes:
                 self.baralho.append(i+j)
+
+    def shuffle(self):
+        random.shuffle(self.baralho)
+
     
 baralho = Baralho()
 print(len(baralho.baralho))
